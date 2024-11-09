68f825b955c15ec90d576f9b4edd0e5e6b26b164
YOUR_NAME <YOUR_EMAIL>
2024-11-09 11:11:33 +0200

    6 урок

--- women/urls.py
+++ women/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import index, categories
+from django.urls import path, re_path, register_converter
+from .views import index, categories, categories_by_slug, archive
+from . import converters
+
+register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
     path('', index),
-    path('cats/', categories),
+    path('cats/<int:cat_id>/', categories),
+    path('cats/<slug:cat_slug>/', categories_by_slug),
+    path('archive/<year4:year>/', archive),
 ]
