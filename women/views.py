b832fa587bc414871267d55413f76b98aec2605c
YOUR_NAME <YOUR_EMAIL>
2024-11-10 14:52:08 +0200

    14 урок - наследование шаблонов

--- women/views.py
+++ women/views.py
@@ -4,49 +4,49 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpRespons
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify  # для фильтрации
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Добавить статью', 'url_name': 'addpage'},
+    {'title': 'Обратная связь', 'url_name': 'contact'},
+    {'title': 'Войти', 'url_name': 'login'},
+]
 
-class MyClass:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True},
+]
 
 # Create your views here.
 def index(request):
-    '''
-    t = render_to_string('women/index.html')
-    return HttpResponse(t)🥰
-    '''
-    data = {'title':'главная страница?',
+    data = {
+            'title':'Главная страница',
             'menu':menu,
-            'float': 28.56,
-            'lst': [1, 2, 3, 'abc'],
-            'set': {1, 2, 3},
-            'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
-            'obj': MyClass(10, 20),
-            'url': slugify('the main PAGE')}
+            'posts': data_db,
+            }
     return render(request, 'women/index.html', context=data)
 
 
 def about(request):
-    return render(request, 'women/about.html', {'title':'О сайте'})
+    return render(request, 'women/about.html', {'title':'О сайте',
+                                                'menu': menu})
 
 
-def categories(request, cat_id):
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>id: {cat_id}</p>')
-
-
-def categories_by_slug(request, cat_slug):
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
-
-def archive(request, year):
-    if year > 2023:
-        uri = reverse('cats', args=('sport', ))
-        return redirect(uri)
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id: {post_id}")\
     
-    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
+
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
