7a1bf0000be17b1f482e8f6621c04af47d71b868
YOUR_NAME <YOUR_EMAIL>
2024-12-15 20:19:16 +0200

    15 урок

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x)rpvgweq_w^1(l_3e9)qpj*p-$jr@6h71u=ca8oueqh_+0wn-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# STATICFILES_DIR = [ BASE_DIR / 'sitewomen/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
